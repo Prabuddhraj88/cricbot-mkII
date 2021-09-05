b673da00232c8b2e7a0c326399c99c700d727c0b
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2021-09-05 21:03:54 +0530

    [+] Added getPlayer, getScore module and few more tiny changes

--- cricbotlib2/cricbotlib2.py
+++ cricbotlib2/cricbotlib2.py
@@ -1,3 +1,4 @@
+from typing import get_args
 import requests, json
 
 BASE_URL = "https://hs-consumer-api.espncricinfo.com"
@@ -6,12 +7,13 @@ HEAD_URL = BASE_URL + "/v1/pages/matches/"
 class URLS():
     lang="?lang=en"
     matches = ["scheduled", "live", "result"]
-    home = "home"
+    home = "/home"
     scorecard = "scorecard"
     statistics="statistics"
     team_players= "team-players"
     sid = "&seriesId="
     mid = "&matchId="
+    imgProvSv = "https://p.imgci.com"
 
 def get_schedules(type_index:int, limit:int):
     url=HEAD_URL + URLS.matches[type_index] + URLS.lang
@@ -43,3 +45,76 @@ def get_schedules(type_index:int, limit:int):
         container.append((mid, sid, series_name, versus, ground, datentime, title, state, status))
     return container[-5:]
 
+
+def get_player(sid:int, mid:int, player_index:int, team_index:int):
+    url = HEAD_URL[:-3] + URLS.home + URLS.lang + URLS.sid + str(sid) + URLS.mid + str(mid)
+    response = requests.get(url).json()
+    team = response["content"]["matchPlayers"]["teamPlayers"][team_index]
+    player = team["players"][player_index]
+    team = team["team"]
+    roles = {
+        "P": "Player",
+        "WK": "Wicket Keeper",
+        "C": "Captain",
+        "VC": "Vice Captain"        
+    }
+    role = roles[player["playerRoleType"]]
+    team_name = team["longName"]
+    team_color = team["primaryColor"]
+    team_logo = URLS.imgProvSv + team["image"]["url"]
+    player = player["player"]
+    player_name = player["longName"]
+    player_gender = player["gender"]
+    player_playingRole = player["playingRole"]
+    try:player_battingStyle = player["longBattingStyles"][0]
+    except IndexError: player_battingStyle = "Not Available"
+    try:player_bowlingStyles= player["longBowlingStyles"][0]
+    except IndexError: player_bowlingStyles = "Not Available"
+    player_image = URLS.imgProvSv + player["image"]["url"]
+    return role, team_name, team_color, team_logo, player_name, player_gender, player_playingRole,\
+    player_battingStyle, player_bowlingStyles, player_image
+
+
+def get_score(sid:int, mid:int):
+    url = HEAD_URL[:-3] + URLS.home + URLS.lang + URLS.sid + str(sid) + URLS.mid + str(mid)
+    response = requests.get(url).json()
+    match = response["match"]
+    mid = match["id"]
+    state = match["state"]
+    datentime = match["startTime"].replace('T', ' ').replace('.000Z', '')
+    title = match["title"]
+    se=match["statusEng"]
+    if match["statusText"] != None:
+        if se == "{{MATCH_START_TIME}}":
+            se = datentime
+        status = se + ": " + match["statusText"]
+    else: status = match["statusEng"]
+    series = match["series"]
+    sid = series["id"]
+    series_name = series["longName"]
+    if match["ground"] != None:
+        ground = match["ground"]["name"]
+    else: ground = "Not Available"
+    teams = match["teams"]
+    versus, score = "", ""
+    for team in teams:
+        versus += team["team"]["slug"] + " v "
+        score += team["score"] 
+        if team["scoreInfo"] != None:
+            score += " (" + team["scoreInfo"] + ")"
+        score += "\n"
+    versus = versus[:-2]
+    livePerformance = response["content"]["livePerformance"]
+    batsmen = livePerformance["batsmen"]
+    bowlers = livePerformance["bowlers"]
+    strikers = ""
+    bowlerssr = ""
+    for batsman in batsmen:
+        strikers += batsman["player"]["longName"] + "- " + str(batsman["runs"]) + \
+            " in " + str(batsman["balls"]) + " (S.R- " + str(batsman["strikerate"]) + ")\n"
+    for bowler in bowlers:
+        bowlerssr += bowler["player"]["longName"] + "- " + str(bowler["conceded"]) + "-" + str(bowler["wickets"]) +\
+            " in " + str(bowler["overs"]) + " overs (E.R- " + str(bowler["economy"]) + ")\n"
+    return mid, sid, series_name, versus, ground, datentime, title, state, status, score, strikers, bowlerssr
+
+    
