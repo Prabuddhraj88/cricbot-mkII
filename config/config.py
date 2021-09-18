1ef732ce2912f6f7a2ae3ec2072adccc4061e5aa
0x0is1 <0x0is1@protonmail.com>
2021-09-19 05:22:58 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
-auth_token = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+auth_token = os.environ.get('BOT_TOKEN')
 bot_prefix = os.environ.get('BOT_PREFIX')
 if bot_prefix == None: bot_prefix = "cb "
 STATUS_REFRESH_TIME = 10
