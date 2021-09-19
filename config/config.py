b34840612c68f2f8538abcaacb85cecce64b8a8b
0x0is1 <0x0is1@protonmail.com>
2021-09-19 14:26:50 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,5 +4,5 @@ bot_prefix = os.environ.get('BOT_PREFIX')
 if bot_prefix == None: bot_prefix = "cb "
 STATUS_REFRESH_TIME = 10
 TRACK_MATCH = os.environ.get('TRACK_MATCH')
-arrows_emojis=['⬆️', '⬇️', '➡️', '⬅️', '🔄', '🔴']
+arrows_emojis=['⬆️', '⬇️', '➡️', '⬅️', '🔄', '🔴', '◻️']
 num_emojis = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
